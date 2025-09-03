39cf2a5302257fa3ee8edd917308acfa9bbc281b
Mansour Emami <mansuurem@gmail.com>
2025-09-03 18:37:26 +0200

    first deployment and Controller class

---
+++ src/main/java/info/emami/ui/controller/UserController.java
@@ -0,0 +1,13 @@
+package info.emami.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user") // https://localhost:8000/users
+public class UserController {
+
+    public String getUser(){
+
+    }
+}
