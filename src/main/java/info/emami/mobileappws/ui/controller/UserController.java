ec73fe16d6b3c0bccf51d5291762a94d0933cc77
Mansour Emami <mansuurem@gmail.com>
2025-09-03 19:54:39 +0200

    GetMapping , PostMapping , PutMapping , Delete Mapping in controller is seted up

---
+++ src/main/java/info/emami/mobileappws/ui/controller/UserController.java
@@ -0,0 +1,31 @@
+package info.emami.mobileappws.ui.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users") //http://localhost:8080/users
+public class UserController {
+
+
+    @GetMapping
+    public String getUser(){
+
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String creatUser(){
+
+        return "Creat user was called";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "delete user was called";
+    }
+}
