d2b05b0d823e8ae2146d382e679b59ebc379a97b
Mansour Emami <mansuurem@gmail.com>
2025-09-07 18:19:29 +0200

    Exception handling2 localMessage

--- src/main/java/info/emami/mobileappws/exceptions/AppExceptionsHandler.java
+++ src/main/java/info/emami/mobileappws/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package info.emami.mobileappws.exceptions;
 
+import info.emami.mobileappws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +9,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
 
-        return new ResponseEntity<>(ex , new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        String errorMessageDescription = ex.getLocalizedMessage();
+
+        if (errorMessageDescription == null) {
+            errorMessageDescription = ex.toString();
+        }
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+
+        return new ResponseEntity<>(errorMessage , new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
